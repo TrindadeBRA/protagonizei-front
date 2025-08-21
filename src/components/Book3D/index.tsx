4bb60c298d6e6ca8474d64bc4b98ae1ebd0f59b4
Lucas Trindade <trindadebra@gmail.com>
2025-08-21 17:06:22 -0300

    fix

--- src/components/Book3D/index.tsx
+++ src/components/Book3D/index.tsx
@@ -41,7 +41,7 @@ export default function Book3D({ className }: Book3DProps) {
 		handleChangeState,
 	} = useAutoFlip({
 		maxFlips: 3,
-		interval: 2000,
+		interval: 1600,
 		enabled: true
 	});
 
