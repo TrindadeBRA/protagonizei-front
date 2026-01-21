9fdc84bb83c6b23d5160ad1e04ecac07ab9b29b4
Lucas Trindade <trindadebra@gmail.com>
2026-01-21 14:10:28 -0300

    fix: adjust padding in LayoutWrapper component for better responsiveness with banner visibility

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -6,7 +6,11 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
   const { isBannerVisible } = useBanner()
   
   return (
-    <div className={`!bg-white !text-black !top-0 ${isBannerVisible ? '!pt-28' : '!pt-16'} overflow-x-hidden`}>
+    <div className={`!bg-white !text-black !top-0 ${
+      isBannerVisible 
+        ? '!pt-20 md:!pt-28' 
+        : '!pt-16'
+    } overflow-x-hidden`}>
       {children}
     </div>
   )
